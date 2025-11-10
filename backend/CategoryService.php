f46b7bfb9cc800dd6091830c75455e86ec51a8b0
TarikKa05 <tarik.korman@stu.ibu.edu.ba>
2025-11-10 20:55:42 +0100

    category service done

---
+++ backend/CategoryService.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'BaseService.php';
+require_once '../dao/CategoryDao.php';
+class CategoryService extends BaseService {
+   public function __construct() {
+
+       $dao = new CategoryDao();
+       parent::__construct($dao);
+   }
+
+   public function createCategory($category) {
+       return $this->dao->createCategory($category);
+   }
+   public function getCategoryById($id) {
+       return $this->dao->getCategoryById($id);
+   }
+   public function getAllCategories() {
+       return $this->dao->getAllCategories();
+   }
+   public function updateCategory($id, $category) {
+       return $this->dao->updateCategory($id, $category);
+   }
+   public function deleteCategory($id) {
+       return $this->dao->deleteCategory($id);
+   }
+}
+
+?>
