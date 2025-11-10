beca6c0896bbb12b34889d7007e311a06e48f572
TarikKa05 <tarik.korman@stu.ibu.edu.ba>
2025-11-10 21:40:56 +0100

    finished User Service

---
+++ backend/services/UserService.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'BaseService.php';
+require_once '../dao/UserDao.php';
+class UserService extends BaseService {
+   public function __construct() {
+
+       $dao = new UserDao();
+       parent::__construct($dao);
+   }
+
+   public function createUser($user) {
+       return $this->dao->createUser($user);
+   }
+   public function getUserById($id) {
+       return $this->dao->getUserById($id);
+   }
+   public function getAllUsers() {
+       return $this->dao->getAllUsers();
+   }
+   public function updateUser($id, $user) {
+       return $this->dao->updateUser($id, $user);
+   }
+   public function deleteUser($id) {
+       return $this->dao->deleteUser($id);
+   }
+   public function getUserByEmail($email) {
+       return $this->dao->getUserByEmail($email);
+   }
+}
+
+?>
