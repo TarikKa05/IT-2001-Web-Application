1ba53b0496dd1e251536af3eb6c7a2eec9ca6e1b
TarikKa05 <tarik.korman@stu.ibu.edu.ba>
2025-11-10 21:21:10 +0100

    finished product category service

---
+++ backend/services/ProductCategoryService.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'BaseService.php';
+require_once '../dao/ProductCategoryDao.php';
+class ProductCategoryService extends BaseService {
+   public function __construct() {
+
+       $dao = new ProductCategoryDao();
+       parent::__construct($dao);
+   }
+
+   public function createProductCategory ($productCategory) {
+       return $this->dao->createProductCategory($productCategory);
+   }
+   public function getProductCategoryById($id) {
+       return $this->dao->getProductCategoryById($id);
+   }
+   public function  getAllProductCategories() {
+       return $this->dao->getAllProductCategories();
+   }
+   public function  updateProductCategory($id, $productCategory) {
+       return $this->dao->updateProductCategory($id, $productCategory);
+   }
+   public function deleteProductCategory($id) {
+       return $this->dao->deleteProductCategory($id);
+   }
+}
+
+?>
