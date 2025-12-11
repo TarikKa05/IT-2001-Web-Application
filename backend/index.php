44bba0d5a2e976fa276dcf102bd494fc0b0a7381
TarikKa05 <tarik.korman@stu.ibu.edu.ba>
2025-12-11 23:20:09 +0100

    jwt

--- backend/index.php
+++ backend/index.php
@@ -1,7 +1,11 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 require 'vendor/autoload.php'; //run autoloader
 
@@ -22,6 +26,37 @@ Flight::register('productService', 'ProductService');
 
 require_once __DIR__ . '/services/UserService.php';
 Flight::register('userService', 'UserService');
+
+require 'rest/services/AuthService.php';
+Flight::register('auth_service', "AuthService");
+require_once __DIR__ .'rest/routes/AuthRoutes.php';
+
+// This wildcard route intercepts all requests and applies authentication checks before proceeding.
+Flight::route('/*', function() {
+   if(
+       strpos(Flight::request()->url, '/auth/login') === 0 ||
+       strpos(Flight::request()->url, '/auth/register') === 0
+   ) {
+       return TRUE;
+   } else {
+       try {
+           $token = Flight::request()->getHeader("Authentication");
+           if(!$token)
+               Flight::halt(401, "Missing authentication header");
+
+
+           $decoded_token = JWT::decode($token, new Key(Config::JWT_SECRET(), 'HS256'));
+
+
+           Flight::set('user', $decoded_token->user);
+           Flight::set('jwt_token', $token);
+           return TRUE;
+       } catch (\Exception $e) {
+           Flight::halt(401, $e->getMessage());
+       }
+   }
+});
+
 
 require_once __DIR__ . '/routes/OrderRoutes.php';
 require_once __DIR__ . '/routes/CategoryRoutes.php';
